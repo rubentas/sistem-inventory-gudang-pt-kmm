d024584faff54f164804caef19b12f74ccfd8cf7
Ruben Tri Ardian Saputra <rubentriardians@gmail.com>
2026-06-22 00:26:51 +0800

    perbaikan sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -178,7 +178,7 @@
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M19 21H5a2 2 0 01-2-2V5a2 2 0 012-2h11l5 5v11a2 2 0 01-2 2z"></path>
                 </svg>
-                Supplier
+                Data Supplier
               </a>
               <a href="{{ route('admin.wilayah') }}"
                 class="flex items-center gap-3 px-4 py-2 rounded-xl text-sm transition {{ request()->routeIs('admin.wilayah') ? 'bg-blue-100 text-blue-700 font-semibold' : 'text-slate-500 hover:bg-slate-100 hover:text-slate-800' }}">
@@ -187,7 +187,7 @@
                     d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z">
                   </path>
                 </svg>
-                Wilayah
+                Data Wilayah
               </a>
               <a href="{{ route('admin.data-sales') }}"
                 class="flex items-center gap-3 px-4 py-2.5 rounded-xl text-sm transition-all duration-300
